6fdc60910416b3cbe553b5e8bb497baf3690b7a0
dareenkhanash <dareenhanash1988@gmail.com>
2018-03-01 14:54:46 +0200

    edit

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 
 
 function greet(name) {
-    alert("Hello" +" "+ $("#theName").val());
+    alert("Hello" +" "+ $("#theName").val()+"we hope  you enjoy");
 }
 
 function BMI(kg,cm){
    var x=Math.pow(kg,2) 
 return cm/x
 }
+
 
 
 
